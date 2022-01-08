2f886eda267cad611fee2ab71e188dbae09ad1cf
shawnSFU <shawn.noruzi@gmail.com>
2022-01-08 14:57:44 -0800

    header image

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -84,35 +84,35 @@ const blackBox = {
     height: "100%",
     bottom: 0,
   },
-  // animate: {
-  //   height: 0,
-  //   transition: {
-  //     when: "afterChildren",
-  //     duration: 1.5,
-  //     ease: [0.87, 0, 0.13, 1],
-  //   },
-  // },
+  animate: {
+    height: 0,
+    transition: {
+      when: "afterChildren",
+      duration: 1.5,
+      ease: [0.87, 0, 0.13, 1],
+    },
+  },
 };
 
 const textContainer = {
   initial: {
     opacity: 1,
   },
-  // animate: {
-  //   opacity: 0,
-  //   transition: {
-  //     duration: 0.3,
-  //     when: "afterChildren",
-  //   },
-  // },
+  animate: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+      when: "afterChildren",
+    },
+  },
 };
 
 const text = {
   initial: {
-    y: 40,
+    y: -30,
   },
   animate: {
-    y: 80,
+    y: 125,
     transition: {
       duration: 1.5,
       ease: [0.87, 0, 0.13, 1],
@@ -137,13 +137,16 @@ const InitialTransition = () => {
         document.body.classList.remove("overflow-hidden")
       }
     >
-      <motion.svg variants={textContainer} className="absolute z-50 flex full-width">
+      <motion.svg
+        variants={textContainer}
+        className="absolute z-50 flex w-full"
+      >
         <pattern
           id="pattern"
           patternUnits="userSpaceOnUse"
           width={1500}
-          height={800}
-          className="text-white"
+          height={1500}
+          className="text-white w-full"
         >
           <rect className="w-full h-full fill-current" />
           <motion.rect
@@ -155,12 +158,30 @@ const InitialTransition = () => {
           className="text-4xl font-bold"
           textAnchor="middle"
           x="50%"
-          y="40%"
+          y="75%"
           style={{ fill: "url(#pattern)" }}
         >
-          Logan Wong
-          Fitness Coach
-  
+          FITNESS
+        </text>
+
+        <text
+          className="text-4xl font-bold"
+          textAnchor="middle"
+          x="50%"
+          y="45%"
+          style={{ fill: "url(#pattern)" }}
+        >
+          -
+        </text>
+
+        <text
+          className="text-4xl font-bold"
+          textAnchor="middle"
+          x="50%"
+          y="20%"
+          style={{ fill: "url(#pattern)" }}
+        >
+          LOGAN WONG
         </text>
       </motion.svg>
     </motion.div>
